1672628856de5b73841704f058ebf508dbe6308a
Samuel Knight <knight.samuel@outlook.com>
2018-05-10 22:56:16 -0700

    Added function to parse through tree and pick out food preferences (allergy info). Changed format of returned dictionary such that each item is a dictionary containing a list of preferences/allergies associated with each item.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -22,9 +22,10 @@ def clean_list(food_list):
 #   Adds new key if food time encountered
 #   Appends to list under current food time if food item
 # Input: A list of food items and food times
-# Returns a dictionary -- each key is menu times, value is a list of menu items available at that time
+# Returns a dictionary -- each key is menu times, value is an inner dictionary of menu items available at that time
+#                                                   value of inner dictionary is list of food preferences (allergies)
 
-def make_food_dict(food_list):
+def make_food_dict(food_list, soup):
     section_indexes = [food_list.index('Breakfast'), food_list.index('Lunch'),
                        food_list.index('Dinner'), food_list.index('Late Night')]
 
@@ -33,10 +34,33 @@ def make_food_dict(food_list):
     for i in range(len(food_list)):
         if i in section_indexes:
             food_section = food_list[i]
-            items_dict[food_section] = []
+            items_dict[food_section] = {}
         else:
-            items_dict[food_section].append(food_list[i])
+            items_dict[food_section][food_list[i]] = []
+    items_dict = add_preferences(items_dict, soup)
     return items_dict
+
+# Traverses through html tree picking out each img tag -- associated with each preference/allergy image
+# Get food item associated with preference and add each preference to the food dictionary
+# Input: Item dictionary of foods, Soup of html tree
+# Returns the updated item dictionary with each food item's preference added
+def add_preferences(items_dict, soup):
+    food_objects = set()
+    for obj in soup.find_all('img'):
+        food_blob = obj.parent.parent
+        food_objects.add(food_blob)
+
+    for obj in food_objects:
+        food_item = re.findall(r"<span .*\">(.*)</span>", str(obj))[0]
+        food_preferences = re.findall(r"src=\"LegendImages/(.*).gif", str(obj))
+        for menu_time in items_dict:
+            for menu_item in items_dict[menu_time]:
+                if food_item == menu_item:
+                    for preference in food_preferences:
+                        items_dict[menu_time][menu_item].append(preference)
+
+    return items_dict
+
 
 
 # Main function for getting menu
@@ -62,8 +86,10 @@ def get_menu(date, locationNum, locationName):
         r = requests.get(url)
 
         soup = BeautifulSoup(r.text, 'html.parser')
-        word_lines = soup.get_text().splitlines()
-        stripped_words = [word.strip() for word in word_lines if not word.strip() == '']
+
+        stripped_words = []
+        for string in soup.stripped_strings:
+            stripped_words.append(string)
 
         # The final_word is the entry after the last food item
         final_word = "The nutrient composition of food may vary due to genetic, " \
@@ -78,7 +104,7 @@ def get_menu(date, locationNum, locationName):
         food_items_dirty = stripped_words[food_items_begin:food_items_end]
 
         food_items_clean = clean_list(food_items_dirty)
-        menu_dict = make_food_dict(food_items_clean)
+        menu_dict = make_food_dict(food_items_clean, soup)
 
         return menu_dict
 
@@ -87,7 +113,7 @@ def get_menu(date, locationNum, locationName):
 def get_menu_url(date, locationNum, locationName):
     date_string = date['month'] + "%2F" + date['day'] + "%2F" + date['year']
     locationName = re.sub(' ', '+', locationName)
-    locationName = re.sub('and', '%26', locationName) #replace the and in College Nine and Ten with html code
+    locationName = re.sub('and', '%26', locationName) #replace the 'and' in College Nine and Ten with html code
     menu_url = re.sub('<date>', date_string, base_url)
     menu_url = re.sub('<loc_num>', locationNum, menu_url)
     menu_url = re.sub('<loc_name>', locationName, menu_url)
